6603106fdb9f90b472e313d96a1d4e648f0c6511
jjasonlan <jasonlan@utopialabs.com>
2023-12-05 10:29:35 -0800

    feat: nft gated experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,58 @@
+import { useEffect, useState } from 'react';
 import { ethers } from 'ethers'
 import { useConnectWallet } from "@web3-onboard/react"
+import NFT_ABI from "./NFT_ABI.json"
 import "./App.css"
 
+const NFT_CONTRACT_ADDRESS = '0xdAfB9d117B585E406A74E84977Fa82DdEE8B0a32';
+
 function App() {
+  const [{ wallet, connecting }, connect, disconnect] = useConnectWallet();
+  const [walletHasNFT, setWalletHasNFT] = useState(false);
+  
+  useEffect(() => {
+    if (!wallet) return
+    const checkNFTStatus = async () => {
+      const res = await hasMintedNFT()
+      setWalletHasNFT(res)
+    };
+    
+    checkNFTStatus()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [wallet])
 
-  const [{ wallet, connecting }, connect, disconnect] = useConnectWallet()
-
-  // create an ethers provider
-  let ethersProvider
-
-  if (wallet) {
-    // if using ethers v6 this is:
-    ethersProvider = new ethers.BrowserProvider(wallet.provider, 'any')
+  async function hasMintedNFT() {
+    const ethersProvider = new ethers.BrowserProvider(wallet.provider, 'sepolia')
+    const contract = new ethers.Contract(NFT_CONTRACT_ADDRESS, NFT_ABI, ethersProvider);
+    const res = await contract.balanceOf(wallet.accounts[0].address)
+    return res && parseInt(res) === 1
   }
-
+  
   return (
     <div className='container'>
-      <button disabled={connecting} onClick={() => (wallet ? disconnect(wallet) : connect())}>
+      <h1>Capsule BlockNative Demo App</h1>
+      <p>An NFT-gated experience, powered by Capsule via Blocknative's <a href="https://docs.blocknative.com/onboard">Web3Onboard</a>.</p>
+      <p>Unlock cross-app superpowers with Capsule!</p>
+      <button className="connect-button" disabled={connecting} onClick={() => (wallet ? disconnect(wallet) : connect())}>
         {connecting ? 'connecting' : wallet ? 'disconnect' : 'connect'}
       </button>
+      {wallet && <div className='content'>
+        {walletHasNFT ? (
+          <>
+            <img src="Eastern_Crown_(Heraldry).svg" alt="crown for being awesome"/>
+            <span>
+              You've got the Capsule NFT! You can access our secret exclusive content. :)
+            </span>
+          </>
+        ) : (
+          <>
+            <img src="Red_X.svg" alt="error icon"/>
+            <span>
+              You are missing the Capsule NFT! Please visit <a href="https://demo.beta.usecapsule.com/">https://demo.beta.usecapsule.com/</a> to mint.
+            </span>
+          </>
+        )}
+      </div>}
     </div>
   );
 }
--- src/AppWithProvider.jsx
+++ src/AppWithProvider.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { Web3OnboardProvider, init } from '@web3-onboard/react'
-import injectedModule from '@web3-onboard/injected-wallets'
 import capsuleModule from '@web3-onboard/capsule'
 import App from "./App"
 
 const blocknativeApiKey = '1730eff0-9d50-4382-a3fe-89f0d34a2070'
 const CAPSULE_API_KEY = "d0b61c2c8865aaa2fb12886651627271";
 
-const injected = injectedModule()
 const capsule = capsuleModule({
   environment: "BETA",
   apiKey: CAPSULE_API_KEY,
@@ -16,30 +14,20 @@ const capsule = capsuleModule({
 // initialize Onboard
 const web3Onboard = init({
   apiKey: blocknativeApiKey,
-  wallets: [injected, capsule],
+  wallets: [capsule],
   chains: [
     {
-      id: '0x5',
+      id: 11155111,
       token: 'ETH',
-      label: 'Goerli',
-      rpcUrl: `https://goerli.infura.io/v3/asdf`
-    },
-    // bug with sepolia?
-    // {
-    //   id: '0xaa36a7',
-    //   token: 'ETH',
-    //   label: 'Sepolia',
-    //   rpcUrl: 'https://sepolia.infura.io/v3/961364684c7346c080994baab1469ea8'
-    // }
+      label: 'Sepolia',
+      rpcUrl: 'https://sepolia.infura.io/v3/961364684c7346c080994baab1469ea8'
+    }
   ],
   appMetadata: {
     name: 'Capsule Blocknative Demo',
     icon: 'capsule-logo.svg',
     logo: 'capsule-logo.svg',
     description: 'Capsule Demo app using Onboard',
-    recommendedInjectedWallets: [
-      { name: 'MetaMask', url: 'https://metamask.io' }
-    ]
   }
 })
 
